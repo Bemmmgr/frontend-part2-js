6b2514eda1aab5bc9ee200fc2984f48a5fc64fc0
Bemmmgr <lix0230@gmail.com>
2025-04-05 22:38:40 +0200

    finish part of section 8

--- a-my-js-practice/08-Behind-the-Scenes/script.js
+++ a-my-js-practice/08-Behind-the-Scenes/script.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const firstName = 'steven';
+
+
+function calAge (nowYear, birthYear) {
+    // console.log(firstName);
+    const age = nowYear - birthYear;
+
+    function printAge() {
+        let output = `${firstName}, You are ${age}, born in ${birthYear}.`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1999) {
+            var millenial = true;       // function scope
+            const firstName = 'bemmgr';
+
+            const str = `Ohh, u are a millenial, ${firstName}`;
+            console.log(str);
+
+            // function are block scope
+            function add (a, b) {
+                return a + b;
+            }
+
+            const output = 'NEW OUTPUT!';
+        }
+
+        // add(2, 3);
+
+    }
+
+    printAge();
+    return age;
+}
+
+// calAge(2025, 1999);
+
+// 08-008 Hoisting & TDZ
+// variables
+console.log(me);        // undefined
+// console.log(job);       // in TDZ: Uncaught ReferenceError:
+// console.log(year);
+
+var me = 'Xiangyu';
+let job = 'student';
+const year = 1990;
+
+// functions
+console.log(addDecl(2, 3));     // 5
+// console.log(addExpr(2, 3));     // Uncaught ReferenceError
+// console.log(addArrow(2, 3));        // Uncaught ReferenceError
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);       // true
+
