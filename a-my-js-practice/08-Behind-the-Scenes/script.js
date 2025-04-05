3d3a869577d23fadc58c1edac9867714ccbfe9c2
Bemmmgr <lix0230@gmail.com>
2025-04-06 00:18:38 +0200

    sencond part of section 8

--- a-my-js-practice/08-Behind-the-Scenes/script.js
+++ a-my-js-practice/08-Behind-the-Scenes/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const firstName = 'steven';
-
+// scope of (variables, functions)
+// const firstName = 'steven';
 
 function calAge (nowYear, birthYear) {
     // console.log(firstName);
@@ -65,4 +65,85 @@ var x = 1;
 let y = 2;
 const z = 3;
 console.log(x === window.x);       // true
+
+// 08010 - this keyword
+// depand on how the function is called
+console.log(this);      // window object
+
+const calAge2 = function (birthYear) {
+    console.log(2025 - birthYear);
+    console.log(this);      //undefined
+}
+
+const calAgeArrow = (birthYear) => {
+    console.log(2025 - birthYear);
+    console.log(this);      // window - for arrow funtion no this keyword, point to its parent keyword
+}
+
+calAge2(2000);
+calAgeArrow(2000);
+
+const steven = {
+    age: 26,
+    job: 'student',
+    calAge: function () {
+        console.log(this);
+        console.log(2025 - this.age);
+    }
+}
+
+steven.calAge();        // Object { age: 26, job: "student", calAge: calAge() }
+
+const matilda = {
+    age: 2017,
+
+}
+
+matilda.calAge = steven.calAge;     // method borrowing
+console.log(matilda);       // Object { year: 2017, calAge: calAge() }
+matilda.calAge();
+
+const f = steven.calAge;
+// f();    // undefined
+
+
+// 08012 
+// var firstName = 'Matilda';
+const bemmgr = {
+    year: 1990,
+    firstName: 'Steven',
+    calAge: function () {
+        // console.log(this);
+        console.log(2025 - this.year);
+
+        // solution 1
+        // const self = this;
+        // const isMillenial = function (){
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <=1996);
+        // };
+        // isMillenial();
+
+        // solution 2
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <=1996);
+        };
+        isMillenial();
+    },
+
+    greet: () => console.log(`Hey! ${this.firstName}`),
+};
+bemmgr.greet();     // Hey! undefined. arrow function does not have this keyword. use this from its surrounding - parent this keyword global
+// after var firstName = 'Matilda'; creates this kind of properties on global object
+
+bemmgr.calAge();
+
+// arguments keyword
+const addExpr2 = function (a, b) {
+    console.log(arguments);
+    return a + b;
+}
+addExpr2(2, 5);     // Arguments { 0: 2, 1: 5, … } only exist in regular functions
+
 
