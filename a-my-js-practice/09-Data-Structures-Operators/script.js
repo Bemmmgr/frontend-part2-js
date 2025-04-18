a5848868fb074ce93a3b9629a124268960c1a29b
bemmGr <123631107+Bemmmgr@users.noreply.github.com>
2025-04-18 23:15:41 +0200

    part 1 of section 9

--- a-my-js-practice/09-Data-Structures-Operators/script.js
+++ a-my-js-practice/09-Data-Structures-Operators/script.js
@@ -44,4 +44,75 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function(starterIndex, mainIndex){
+    // this 引用调用该方法的对象: restaurant
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function(obj){
+    console.log(obj);
+  },
 };
+
+// 09003 destructuring 解构
+const arr = [2, 4, 6];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y,z);    // destructuring 2, 4, 6
+
+
+let [first, , second] = restaurant.categories;
+console.log(first, second);     // Italian Vegetarian
+[first, second] = [second, first];
+console.log(first, second);     // Vegetarian Italian
+
+// receive 2 return values
+const [starter, mainCourse] = restaurant.order(1, 2);
+console.log(starter, mainCourse);
+
+// nested destructuring   嵌套解构
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, ,[j, k]] = nested;
+console.log(i, j, k);     // 2, 5, 6
+
+// default values
+const[p, q, r] = [8, 9];
+console.log(p, q, r);     // 8, 9, undefined
+
+
+// 09005 destructuring objects
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { mainMenu: Menu, starterMenu: starters = []} = restaurant;
+console.log(Menu, starters);
+
+// mutating variables
+let m = 111;
+let n = 999;
+const obj = {
+  m: 25,
+  n: 17,
+  o: 12
+};
+
+({m, n} = obj);       // 注意括号
+console.log(m, n);    // 25, 17
+
+// nested objects
+let {fri: {open, close}} = openingHours;
+let {fri: {open: o, close: d},} = openingHours;
+console.log(open, close);     // fri: {open: 11, close: 23}  11 23
+console.log(o, d);
+
+
+
