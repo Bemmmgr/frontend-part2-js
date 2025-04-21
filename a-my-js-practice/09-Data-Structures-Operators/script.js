a556e6989a74278c7bb1d0a3aa8f1a9d71ad43d2
bemmGr <123631107+Bemmmgr@users.noreply.github.com>
2025-04-21 17:32:35 +0200

    video progress section 2 start point

--- a-my-js-practice/09-Data-Structures-Operators/script.js
+++ a-my-js-practice/09-Data-Structures-Operators/script.js
@@ -50,9 +50,20 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function(obj){
-    console.log(obj);
+  orderDelivery: function({starterIndex=1, mainIndex=0, time, address})     // 使用对象的解构
+  {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
+
+  orderPista: function(ing1, ing2, ing3){
+    console.log(`Here is ur pista with ${ing1}, ${ing2} and ${ing3},`);
+  },
+
+  orederPasta: function (mainIng, ...restIng) {
+    console.log(mainIng);
+    console.log(restIng);
+  },
+
 };
 
 // 09003 destructuring 解构
@@ -114,5 +125,130 @@ let {fri: {open: o, close: d},} = openingHours;
 console.log(open, close);     // fri: {open: 11, close: 23}  11 23
 console.log(o, d);
 
+restaurant.orderDelivery({
+  time: '20:39',
+  address: 'klooosterpoort',
+  mainIndex: 2,
+  starterIndex: 3,
+});
 
+restaurant.orderDelivery({
+  address: 'klooosterpoort',
+  mainIndex: 1,
+});       // 有默认值显示，无默认值undefine
+
+
+// 09006 - the spread operator
+const arrX = [7, 8, 9]
+const badArrX = [5, 6, arrX[0], arrX[1], arrX[2]];
+console.log(badArrX);
+
+const newArrX = [5, 6, ...arrX];    //take all elements and write individually 拿出所有元素拼接
+console.log(newArrX);        // [5, 6, 7, 8, 9]
+console.log(...newArrX);    // 5 6 7 8 9
+
+const newMenu = [...restaurant.mainMenu, 'Goncci'];
+console.log(newMenu);       // ['Pizza', 'Pasta', 'Risotto', 'Goncci'] - new arr
+
+// copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// join 2 arrays
+const menuJ = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menuJ);
+
+// Iterables: arrays, strings, maps, sets, Not objects
+const str = 'steven';
+const letters = [...str, '', 'A'];
+console.log(letters);     // ['s', 't', 'e', 'v', 'e', 'n', '', 'A']
+
+// connot use in ${}
+//console.log(`${...str} Li`);
+
+// const ingredients = 
+// [ prompt("Let\'s make psita! ingredient 1 ?"), 
+//   prompt("ingredient 2 ?"),
+//   prompt("ingredient 3 ?"),
+// ]
+// console.log(ingredients);         // 手动输入: ['a', 'b', 'c']
+
+// same result
+// restaurant.orderPista(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPista(...ingredients);
+
+
+// Objects
+const newRestaurant = {...restaurant, founder: 'Li family', foundedIn: 1998}
+console.log(newRestaurant);
+
+const copyRestaurant = {...restaurant};
+copyRestaurant.name = 'Ristorante Rome';
+console.log(copyRestaurant.name, '/', restaurant.name);    // Ristorante Rome / Classico Italiano
+
+// spread  
+const arrr = [1, 2, ...[3, 4]];
+console.log(arrr);      // [1, 2, 3, 4]
+
+const [v,s, ...others] = [1, 2, 3, 4, 5];
+console.log(v, s, others);          // 1 2 [3, 4, 5]
+
+const [Pizza, ,Risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+console.log(Pizza, Risotto, ...otherFood);      //Pizza Risotto Focaccia Bruschetta Garlic Bread Caprese Salad
+
+// objects
+const {sat, ...otherTime} = restaurant.openingHours;
+console.log(sat, otherTime);
+
+// functions
+const add = function(...numbersNew){
+  // ...numbers 整合成一个[]
+  let sum = 0;
+  for (let y = 0; y < numbersNew.length; y++)
+    sum += numbersNew[y];
+  console.log(sum);
+}
+
+add(4, 5, 2, 7, 9, 3);
+
+const nn = [15, 76, 21];
+console.log(add(...nn));
+
+//
+restaurant.orederPasta('mushrooms', 'onions', 'cheese', 'bacon');
+restaurant.orederPasta('cheese');
+
+// 09008 short circuiting && and    短路
+// Use ANY data type, return ANY data type, short-circuiting
+console.log('-------------OR--------------');
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 8;
+const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guest1);
+
+const guest2 = restaurant.numGuests || 6;
+console.log(guest2);
+
+console.log('-------------AND--------------');
+console.log(0 && 'li');   // 0
+console.log(3 && 'li');   // li
+
+console.log(0 && '' && 'Hello' && 23 && null);    //0
+
+// practical
+if(restaurant.orderPista) {
+  restaurant.orderPista('pussy');
+}
+restaurant.orderPista && restaurant.orderPista('mushrooms');
+
+console.log('-------------Nullish--------------');
+// Only when values are null, undefined, then contunue. Not 0 or ''
+restaurant.numGuests = 0;
+const guest3 = restaurant.numGuests ?? 3;
+console.log(guest3);        // 0
 
