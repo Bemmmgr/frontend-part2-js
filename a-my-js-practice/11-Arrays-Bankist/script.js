7b3942310c121d6253e8870e3ff98f758c5362c7
bemmGr <123631107+Bemmmgr@users.noreply.github.com>
2025-09-17 01:24:23 +0200

    new section 11 start & prepare for later section

--- a-my-js-practice/11-Arrays-Bankist/script.js
+++ a-my-js-practice/11-Arrays-Bankist/script.js
@@ -74,3 +74,48 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// 11003 - Array methods
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));          // ['c', 'd', 'e']
+console.log(arr.slice(1,3));        // ['b', 'c']
+console.log(arr.slice(-3));         // ['c', 'd', 'e']
+console.log(arr.slice(1, -2));      // ['b', 'c']
+console.log([...arr]);              // ['a', 'b', 'c', 'd', 'e']
+
+// SPLICE
+//console.log(arr.splice(2));       // ['c', 'd', 'e']  original: ['a', 'b']
+arr.splice(-1);                     // remove last one:['a', 'b', 'c', 'd']
+arr.splice(1, 2)                    // ['a', 'd']
+console.log(arr);             
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());              // ['f', 'g', 'h', 'i', 'j']
+console.log(arr2);                        // original: ['f', 'g', 'h', 'i', 'j']
+
+// CONCAT
+const sentence = arr.concat(arr2);
+console.log(sentence);                    // ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+console.log([...arr, ...arr2]);           // same
+
+// JOIN
+console.log(sentence.join(' - '));        // string: a - b - c - d - e - f - g - h - i - j
+
+// check MDN every time
+
+// 11004 new AT method
+const arrAT = [23, 74, 62];
+console.log(arrAT[0]);            // 23
+console.log(arrAT.at(1));         // 74
+
+// 3 ways to get last element
+console.log(arrAT[arrAT.length - 1]);       // 62
+console.log(arrAT.slice(-1)[0]);
+console.log(arrAT.at(-1));
+// suitable for strings
+console.log('I am Steven Li'.at(-1));       // i
+
+
+// 11005 Looping arrays: forEach
