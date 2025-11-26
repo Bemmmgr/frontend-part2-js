71a7fdcfb0a215af9ecddede59a6441f7192bcea
Xiangyu Li - PC <lix0230@gamil.com>
2025-11-27 00:55:16 +0100

    learning ajax call & fetch then promise, etc..

--- a-my-js-practice/16-Asynchronous/script.js
+++ a-my-js-practice/16-Asynchronous/script.js
@@ -20,31 +20,23 @@ Application Programming Interface - API
 */
 
 // 16005 - First AJAX call XMLhttpRequest
+// 16006 - Request & Response
+// 16007 - Callback Hell - nested callback
 // old school way
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`); // public apis
-  request.send();
-  request.addEventListener('load', function () {
-    console.log(this.responseText); // JSON
-    // convert JSON
-    const data = JSON.parse(this.responseText);
-    const [country] = data;
-    console.log(data); // Array - 0: {name: {…}, tld: Array(1), cca2: 'PT', ccn3: '620', cioc: 'POR', …}
+const renderCountry = function (country, className = '') {
+  //
+  const languages = country.languages
+    ? Object.values(country.languages).join(', ')
+    : 'N/A';
 
-    //
-    const languages = country.languages
-      ? Object.values(country.languages).join(', ')
-      : 'N/A';
+  const currencies = country.currencies
+    ? Object.values(country.currencies)
+        .map(c => c.name)
+        .join(', ')
+    : 'N/A';
 
-    const currencies = country.currencies
-      ? Object.values(country.currencies)
-          .map(c => c.name)
-          .join(', ')
-      : 'N/A';
-
-    const html = `
-    <article class="country">
+  const html = `
+    <article class="country ${className}">
         <img class="country__img" src="${country.flags.png}" />
         <div class="country__data">
         <h3 class="country__name">${country.name.common}</h3>
@@ -57,21 +49,80 @@ const getCountryData = function (country) {
         </div>
     </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+/*
+const getCountryAndNeighbour = function (country) {
+  // AJAX call
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`); // public apis
+  request.send();
+  request.addEventListener('load', function () {
+    console.log(this.responseText); // JSON
+    // convert JSON
+    const data = JSON.parse(this.responseText);
+    const [country] = data; // data - 数组
+    console.log(data); // Array - 0: {name: {…}, tld: Array(1), cca2: 'PT', ccn3: '620', cioc: 'POR', …}
+
+    // render country
+    renderCountry(country);
+
+    // get neighbor country - optional chaining - data.borders 不存在 即 undefined
+    const neighbor = country.borders?.[0];
+    if (!neighbor) return;
+
+    // Ajax call 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`); // public apis
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      // console.log(this.responseText);
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // render
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
-getCountryData('nl');
-getCountryData('portugal');
-getCountryData('fra');
+getCountryAndNeighbour('nl');
+*/
 
-// 16005 -
-// 16006 -
-// 16007 -
-// 16008 -
-// 16009 -
-// 160010 -
-// 160011 -
+// 16008 - Promises and fetch API
+// 16009 - Consuming promises
+// 160010 - Chaining promises
+const request = fetch('https://restcountries.com/v3.1/name/netherlands');
+console.log(request); // Promise {<pending>} - Promise = “可链式调用、可集中处理错误”的异步结果容器 - container for asy future value
+
+const getCountryData = function (country) {
+  // country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json()) // asynchronous func
+    .then(data => {
+      const [countryData] = data;
+      renderCountry(countryData);
+      const neighbor = countryData.borders?.[0];
+
+      if (!neighbor) return null;
+
+      // second ajax call - country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+    })
+    .then(response => {
+      if (!response) return null;
+      return response.json();
+    })
+    .then(data => {
+      if (!data) return;
+      renderCountry(data[0], 'neighbour');
+    });
+};
+getCountryData('germany');
+
+// 160011 - Handling rejected promises
 // 160012 -
 // 160013 -
 // 160014 -
